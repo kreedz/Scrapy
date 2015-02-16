8304647c20ac62a417893c0ad685cc3d5e9fb505
kreedz <kreedz@users.noreply.github.com>
2015-02-17 00:24:03 +0500

    add models

---
+++ habrahabr/habrahabr/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.engine.url import URL
+import settings
+
+
+DeclarativeBase = declarative_base()
+
+def db_connect():
+    return create_engine(URL(**settings.DATABASE))
+
+def create_habrahabr_table(engine):
+    DeclarativeBase.metadata.create_all(engine)
+
+def delete_from_habrahabr_table(engine, Session):
+    session = Session()
+    try:
+        session.query(HabrahabrModel).delete()
+        session.commit()
+    except:
+        session.rollback()
+
+
+class HabrahabrModel(DeclarativeBase):
+    __tablename__ = 'habrahabr'
+    id = Column(Integer, primary_key=True)
+    title = Column('title', String)
