69c85248fce3270c194c38fc19e37f3fb518c366
kreedz <kreedz@users.noreply.github.com>
2015-02-21 03:17:51 +0500

    normalize some others types of dates, check in spider if date of msg less predefined date then exit

--- authCheckSpamBanUserSaveInfo/vk/items.py
+++ authCheckSpamBanUserSaveInfo/vk/items.py
@@ -16,16 +16,25 @@ def normalize_date(date):
     date_format = '%d %b %y'
     p = re.compile(ur'((\S+?\s)?(минут|час)\S{,2}\sназад)|сегодня.*', re.UNICODE)
     if p.match(date):
-        return today.strftime(date_format).decode('utf-8').lower()
-    p = re.compile(r'вчера')
+        return today.strftime(date_format).decode('utf-8')
+    p = re.compile(ur'вчера', re.UNICODE)
     if p.match(date):
-        return (today - timedelta(days=1)).strftime(date_format).decode('utf-8').lower()
-    return date
+        return (today - timedelta(days=1)).strftime(date_format).decode('utf-8')
+    p = re.compile(ur'(\d{1,2})\s(\w{3})\s(?:(в)\s\d{1,2}:\d{2}|(\d{4}))', re.UNICODE)
+    m = p.match(date)
+    if m:
+        date = m.group(1) + ' ' +  m.group(2) + ' '
+        if m.group(3) is None:
+            date += m.group(4)[2:]
+        else:
+            date += str(datetime.now().year % 100)
+    first_month_char = date[date.find(' ') + 1]
+    return date.replace(first_month_char, first_month_char.upper())
 
 
 class VkItem(Item):
     id = Field(output_processor=TakeFirst())
     name = Field(output_processor=TakeFirst())
     text = Field(input_processor=MapCompose(remove_tags), output_processor=TakeFirst())
-    date = Field(input_processor=MapCompose(normalize_date), output_processor=TakeFirst())
+    date = Field(output_processor=TakeFirst())
     words = Field()
--- authCheckSpamBanUserSaveInfo/vk/spiders/vkwall.py
+++ authCheckSpamBanUserSaveInfo/vk/spiders/vkwall.py
@@ -4,20 +4,40 @@ from scrapy.http import Request, FormRequest
 from scrapy import log
 from scrapy.contrib.spiders.init import InitSpider
 from scrapy.contrib.loader import ItemLoader
-from vk.items import VkItem
+from scrapy.contrib.loader.processor import TakeFirst, MapCompose
+from vk.items import VkItem, normalize_date
 
 import codecs
 import os
 import json
+import locale
+from datetime import datetime, date, timedelta
 
+
+locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
 
 def get_spam_words_from_file(filename):
     with codecs.open(filename, 'r', encoding='utf-8') as f:
         return [spaw_word.strip() for spaw_word in f.readlines()]
 
+def get_spam_words_from_msg(text, spam_words_file):
+    if text:
+        text = text[0]
+    spam_words = []
+    for spam_word in get_spam_words_from_file(spam_words_file):
+        if spam_word in text:
+            spam_words.append(spam_word)
+    return spam_words
+
 def get_auth_data(filename):
     with open(filename, 'r') as f:
         return json.loads(f.readline())
+
+def get_last_date_to_parse(days):
+    return datetime.now().date() - timedelta(days=days)
+
+def is_date_less_last_date(date, days_to_last_date):
+    return datetime.strptime(date.encode('utf-8'), '%d %b %y').date() < get_last_date_to_parse(days_to_last_date)
 
 
 class VkwallSpider(InitSpider):
@@ -27,8 +47,10 @@ class VkwallSpider(InitSpider):
     login_page = 'https://vk.com/login.php'
 
     txt_dir = 'txt'
-    spam_words = get_spam_words_from_file(os.path.join(txt_dir, 'spam_words.txt'))
+    spam_words_file = os.path.join(txt_dir, 'spam_words.txt')
     auth_data = os.path.join(txt_dir, 'auth_data.txt')
+
+    days_count_to_parse = 5
 
     def init_request(self):
         return Request(url=self.login_page, callback=self.login)
@@ -56,22 +78,17 @@ class VkwallSpider(InitSpider):
         for s in sel:
             wall_text = s.xpath('div[@class="wall_text"]')
             text = wall_text.xpath('div/div[@class="wall_post_text"]').extract()
-            spam_words = self.get_spam_words_from_msg(text)
+            spam_words = get_spam_words_from_msg(text, self.spam_words_file)
             if spam_words:
                 l = ItemLoader(item=VkItem(), selector=s, response=response)
+                date = s.xpath('div[@class="replies"]/div/small/a[1]/span/text()').extract()
+                date = l.get_value(date, MapCompose(normalize_date), TakeFirst())
+                if is_date_less_last_date(date, self.days_count_to_parse):
+                    return
                 l.add_value('id', wall_text.xpath('div/a/@data-from-id').extract())
                 l.add_value('name', wall_text.xpath('div/a/text()').extract())
                 l.add_value('text', text)
-                l.add_xpath('date', 'div[@class="replies"]/div/small/a[1]/span/text()')
+                l.add_value('date', date)
                 l.add_value('words', spam_words)
                 yield l.load_item()
                 #yield Request(get_posts_url, callback=self.parse)
-
-    def get_spam_words_from_msg(self, text):
-        if text:
-            text = text[0]
-        spam_words = []
-        for spam_word in self.spam_words:
-            if spam_word in text:
-                spam_words.append(spam_word)
-        return spam_words
